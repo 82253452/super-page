96eadba7fefdfb0aa6788089013573556a0a1294
yuping <yuping@zhonglanmedia.com>
2020-11-16 21:03:09 +0800

    init

--- config/config.prod.js
+++ config/config.prod.js
@@ -1,6 +1,6 @@
 export default {
   define: {
     // 'process.env.DOMAIN': "http://localhost:8080"
-    'process.env.DOMAIN': "https://mass.doatu.com"
+    'process.env.DOMAIN': "https://super-page.doatu.com/api"
   }
 };
